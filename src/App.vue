fb41083313f6d9244cd9c17e30ed410d86dd0b63
徐旭 <867511126@qq.com>
2025-11-16 19:26:48 +0800

    继续验证

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
+  <h1 class="text-4xl md:text-6xl font-extrabold text-center mt-20
+             bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500
+             text-transparent bg-clip-text drop-shadow-md">
+    Hello World!
   </h1>
 </template>
 
